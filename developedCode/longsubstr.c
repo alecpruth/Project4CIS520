00f56ed2492f4d922b92c778765a89ea84afa9fb
Alec Ruth <alecpruth@gmail.com>
2018-04-22 21:48:36 -0400

    worked on openMp

--- developedCode/longsubstr.c
+++ developedCode/longsubstr.c
@@ -8,8 +8,7 @@
 #include <fcntl.h>
 
 #define BUF_SIZE 10*1024*1024
-#define LINE_COUNT_MAX 3000
-#define LINE_LENGTH_MAX 65536
+#define LINE_COUNT_MAX 10000
 
 unsigned match_count (char *str1, char *str2) 
 {
@@ -25,19 +24,19 @@ unsigned i;
 }
 
 
-unsigned find_longest_substr(char *str1, char *str2, char *write_to)
+unsigned find_longest_substr(char *str1, char *str2, char ** write_to)
 {
 unsigned cnt;
-unsigned longest_length;
+unsigned longest_length = 0;
 char *ptr1 = str1;
     
     for(; *str2; str1 = ptr1, str2++ ) {
         for(; *str1; str1++ )
         {
             cnt = match_count(str1,str2);
-            if(strlen(write_to) < cnt ) {
+            if(longest_length < cnt ) {
                 longest_length = cnt;
-                strncpy(write_to, str1, cnt);
+                *write_to = str1;
             }
         }
     }
@@ -58,7 +57,6 @@ char *longest_substr;
 
     buf = (char *)malloc(BUF_SIZE);
     line_ptrs = (char **)malloc(LINE_COUNT_MAX);
-    longest_substr = (char *)malloc(LINE_LENGTH_MAX);
     memset(buf, 0, BUF_SIZE);
     // printf("Filename is <%s>\n", filename);
     fd = open(filename, O_RDONLY);
@@ -79,14 +77,16 @@ char *longest_substr;
     
     for(int i = 0; line_ptrs[i] != NULL && line_ptrs[i+1] != NULL; i++) {
         //printf("line %d: <%s>\n", i, line_ptrs[i]);
-        memset(longest_substr, 0, LINE_LENGTH_MAX);
-        find_longest_substr(line_ptrs[i], line_ptrs[i+1], longest_substr);
-        printf("<%d> and <%d> : <%s>\n", i, i+1, longest_substr);
+        int longest_length = find_longest_substr(line_ptrs[i], line_ptrs[i+1], &longest_substr);
+        printf("<%d> and <%d> : <", i, i+1);
+        fflush(stdout);
+        write(STDOUT_FILENO, longest_substr, longest_length);
+        printf(">\n");
+        
     }
     
     free(buf);
     free(line_ptrs);
-    free(longest_substr);
     
 }
 
