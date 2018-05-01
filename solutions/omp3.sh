e5a11069784ab00a13b2aed98dd3449da64b547a
Travis <travis14@ksu.edu>
2018-05-01 11:59:55 -0500

    added omp3.sh

--- solutions/omp3.sh
+++ solutions/omp3.sh
@@ -3,15 +3,14 @@
 # FILE: batchscript.c
 # DESCRIPTION:
 #   Simple Slurm job command script for MPI exercise
-#pthreads
+# pthreads
 # AUTHOR: Blaise Barney
 # LAST REVISED: 05/23/17
 ##############################################################################
 
 ##### These lines are for Slurm
 #SBATCH -N 1                      #Number of nodes to use
-#SBATCH -p pReserved              #Workshop queue
-#SBATCH -t 5:00                   #Maximum time required
+#SBATCH -t 60:00                   #Maximum time required
 #SBATCH -o output.%j              #Output file name
 
 #SBATCH --constraint=elves
@@ -20,8 +19,7 @@
 ### Display some diagnostic information
 echo '=====================JOB DIAGNOTICS========================'
 date
-NUMNODES=$(echo 1)
-echo -n 'This machine is ';hostname
+echo -n 'This machine is '; hostname
 echo -n 'My jobid is '; echo $SLURM_JOBID
 echo 'My path is:'
 echo $PATH
@@ -32,12 +30,8 @@ sinfo -s
 
 echo '=====================JOB STARTING=========================='
 
-#Compile an exercise code
-gcc -g -fopenmp 3way-openmp.c
-#Run the code pass in the program to run, ours takes filename, then number of threads and then number of lines
-srun -N1 ./a.out /homes/dan/625/wiki_dump.txt 
-### Issue the sleep so we have time to see the job actually running
-sleep 120
+/usr/bin/time -v /homes/travis14/3way /homes/travis14/wiki_dump.txt 
+
 echo ' '
 echo '========================ALL DONE==========================='
 
